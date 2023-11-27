287d89ae345e6e6d8b1105f5fdb68bc5954c9c3e
Ozan797 <ozan8@hotmail.co.uk>
2023-11-27 19:20:17 +0000

    created and designed homepage

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
-import React from "react";
 import styles from "./Home.module.css";
-import Clouds from "../../assets/Cloudy.svg";
+// import Clouds from "../../assets/Cloudy.svg";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 const Home = () => {
   return (
@@ -10,11 +11,12 @@ const Home = () => {
           <h3 className={styles.head}>Hello, my name is </h3>
           <h1 className={styles.title}>Ozan Gokberk</h1>
           <p className={styles.titleDesc}>
-            I'm Ozan, a skilled software engineer specialized in blockchain
-            technology and proficient in Typescript, NestJS, ReactJS, and
-            Python. My passion lies at the crossroads of blockchain innovation
-            and AI development along with web development, where I craft
-            cutting-edge solutions that redefine technological possibilities.
+            I'm Ozan, a skilled software engineer specialized in Fullstack
+            development and proficient in Typescript, NestJS, ReactJS, and
+            Python. My passion lies at the crossroads of Web development
+            innovation and AI development along with blockchain technology,
+            where I craft cutting-edge solutions that redefine technological
+            possibilities.
           </p>
 
           {/* Buttons */}
@@ -25,11 +27,17 @@ const Home = () => {
             <button>
               <a href="/experience">View Experience</a>
             </button>
+            <a href="https://github.com/Ozan797" target="_blank" rel="noreferrer" id={styles.link}>
+              <GitHubIcon className={styles.githubIcon} />
+            </a>
+            <a href="https://www.linkedin.com/in/ozan-gokberk-bb6939240/" target="_blank" rel="noreferrer">
+              <LinkedInIcon className={styles.linkedInIcon} />
+            </a>
           </div>
         </div>
-        <div className={styles.clouds}>
+        {/* <div className={styles.clouds}>
           <img src={Clouds} alt="Image of clouds" />
-        </div>
+        </div> */}
       </section>
     </>
   );
