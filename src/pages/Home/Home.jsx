7b9fbfdbd9d3224f8e8fd6cc2d7d2e69524cdb35
Ozan797 <ozan8@hotmail.co.uk>
2023-11-27 21:32:30 +0000

    added typewriter effect

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,24 @@
 import styles from "./Home.module.css";
 // import Clouds from "../../assets/Cloudy.svg";
 import GitHubIcon from "@mui/icons-material/GitHub";
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import Typewriter from "typewriter-effect";
 
 const Home = () => {
   return (
     <>
       <section id="landing" className={styles.home}>
         <div className={styles.container}>
-          <h3 className={styles.head}>Hello, my name is </h3>
+          <h3 className={styles.head}>
+            <Typewriter
+              options={{
+                strings: ["Hello, my name is", "Merhaba, benim adim"],
+                autoStart: true,
+                loop: true,
+                delay: 60,
+              }}
+            />
+          </h3>
           <h1 className={styles.title}>Ozan Gokberk</h1>
           <p className={styles.titleDesc}>
             I'm Ozan, a skilled software engineer specialized in Fullstack
@@ -27,10 +37,19 @@ const Home = () => {
             <button>
               <a href="/experience">View Experience</a>
             </button>
-            <a href="https://github.com/Ozan797" target="_blank" rel="noreferrer" id={styles.link}>
+            <a
+              href="https://github.com/Ozan797"
+              target="_blank"
+              rel="noreferrer"
+              id={styles.link}
+            >
               <GitHubIcon className={styles.githubIcon} />
             </a>
-            <a href="https://www.linkedin.com/in/ozan-gokberk-bb6939240/" target="_blank" rel="noreferrer">
+            <a
+              href="https://www.linkedin.com/in/ozan-gokberk-bb6939240/"
+              target="_blank"
+              rel="noreferrer"
+            >
               <LinkedInIcon className={styles.linkedInIcon} />
             </a>
           </div>
