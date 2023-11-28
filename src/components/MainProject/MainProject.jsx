8877fe29fc6544c862a24e1ac094c1bb5994769d
Ozan Gokberk <ozan8@hotmail.co.uk>
2023-11-28 18:16:38 +0000

    created structure of main proejct card

--- src/components/MainProject/MainProject.jsx
+++ src/components/MainProject/MainProject.jsx
@@ -1,8 +1,53 @@
+import styles from "./MainProject.module.css";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import LaunchIcon from "@mui/icons-material/Launch";
 
-const MainProject = () => {
+const MainProject = ({
+  projRank,
+  projName,
+  projDesc,
+  techStack,
+  projectImg,
+  githubLink,
+  websiteLink,
+  videoLink,
+}) => {
   return (
-    <div>MainProject</div>
-  )
-}
+    <div className={styles.container}>
+      {/* Description side of project */}
+      <div className={styles.projectInfo}>
+        <div className={styles.projRank}>
+          <h4>{projRank}</h4>
+        </div>
+        <div className={styles.projName}>
+          <h1>{projName}</h1>
+        </div>
+        <div className={styles.projDesc}>
+          <p>{projDesc}</p>
+        </div>
+        <div className={styles.techStack}>
+          <p>{techStack}</p>
+        </div>
+        <div className={styles.links}>
+          <a href="#">{githubLink}</a>
+          {websiteLink && (
+            <a href={websiteLink}>
+              <LaunchIcon />
+            </a>
+          )}
+          {videoLink && (
+            <a href={videoLink}>
+              <YouTubeIcon />
+            </a>
+          )}
+        </div>
+      </div>
 
-export default MainProject
+      {/* Image side of main project */}
+      <div className={styles.projectImg}>
+        <img src={projectImg} alt="A image" /></div>
+    </div>
+  );
+};
+
+export default MainProject;
--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,11 +1,22 @@
-import styles from "./Projects.module.css"
-
+import styles from "./Projects.module.css";
+import MainProject from "../../components/MainProject/MainProject";
+import Img from "../../assets/react.svg"
 const Projects = () => {
   return (
     <section id={styles.projects}>
-
+      <MainProject
+        projRank="Main Project"
+        projName="JobifyHub"
+        projDesc="Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Consequuntur, animi cumque necessitatibus in recusandae totam
+            repudiandae corporis sequi, sapiente saepe alias rem ipsam adipisci
+            commodi corrupti, tempora quos tenetur quas?"
+        techStack="Nestjs, ReactJS"
+        videoLink="dsd"
+        projectImg="https://images.unsplash.com/photo-1507679799987-c73779587ccf?q=80&w=2671&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      />
     </section>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
